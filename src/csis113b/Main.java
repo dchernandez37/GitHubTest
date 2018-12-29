456e09c31c511941d51990ac057de49708bedd73
David C Hernandez <davidhernandez@Davids-MacBook-Pro.local>
2018-12-29 15:14:05 -0800

    Initial commit

---
+++ src/csis113b/Main.java
@@ -0,0 +1,48 @@
+package csis113b;
+
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        // write your code here
+        int firstNumber = 3;
+        firstNumber++;
+        ++firstNumber;
+        
+        // declare and initialize a second number
+        var secondNumber = 7;
+        var result = firstNumber + secondNumber;
+        var anotherNumber = result++;
+        var lastNumber = ++result;
+    
+        var counter = 0;
+        
+        // create an inner scope to demonstrate garbage disposal
+        {
+            counter++;
+            var hiddenNumber = counter;
+            counter++;
+        }
+        
+        ++counter;
+    }
+    
+    public static boolean max(int first, int second) {
+        return first >= second;
+    }
+    
+    public static boolean min(int first, int second) {
+        return first <= second;
+    }
+    
+    public static int sumNumbersInRange(int start, int stop) {
+        var sum = 0;
+        
+        for(var i = start; i <= stop; i++) {
+            sum += i;
+        }
+        
+        return sum;
+    }
+}
